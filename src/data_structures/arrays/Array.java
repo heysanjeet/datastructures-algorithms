50ede49bf6900908322aefe6ecd63e1a8a9e72eb
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2022-08-06 18:53:48 +0530

    implementation of Arrays data structure.

---
+++ src/data_structures/arrays/Array.java
@@ -0,0 +1,14 @@
+package data_structures.arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        char[] array = {'a', 'b', 'c', 'd'};
+        findValue(array);
+
+    }
+
+    //Method to take char[] as a input parameter and find the value from the array.
+    public static void findValue(char[] array) {
+        System.out.println(array[3]);
+    }
+}
