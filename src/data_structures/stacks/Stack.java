e28c758cdee24267661b66521ba203265c61286e
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2022-08-08 13:10:05 +0530

    implementation of Stack.

--- src/data_structures/stacks/Stack.java
+++ src/data_structures/stacks/Stack.java
@@ -5,10 +5,26 @@ public class Stack {
     private long[] stackArray;
     private int top;
 
-    public Stack(int size){
-        this.maxSize=size;
-        this.stackArray=new long[maxSize];
-        this.top=-1;
+    public Stack(int size) {
+        this.maxSize = size;
+        this.stackArray = new long[maxSize];
+        this.top = -1;
 
+    }
+
+    public void push(long value) {
+        top++;
+        stackArray[top] = value;
+
+    }
+
+    public long pop() {
+        long topValue = stackArray[top];
+        top--;
+        return topValue;
+    }
+
+    public int size() {
+        return top;
     }
 }
---
+++ src/data_structures/stacks/StackClient.java
@@ -0,0 +1,12 @@
+package data_structures.stacks;
+
+public class StackClient {
+    public static void main(String[] args) {
+        Stack stack = new Stack(10);
+        stack.push(25);
+        stack.push(40);
+        stack.push(90);
+        System.out.println(stack.size());
+        System.out.println(stack.pop());
+    }
+}
