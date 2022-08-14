03add3b0b17c74e3b661a28b0c70556534909506
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2022-08-14 14:43:28 +0530

    implementation of SinglyLinkedList.

---
+++ src/data_structures/linked_list/SinglyLLClient.java
@@ -0,0 +1,17 @@
+package data_structures.linked_list;
+
+public class SinglyLLClient {
+    public static void main(String[] args) {
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+        linkedList.insertFirst(10);
+        linkedList.insertFirst(20);
+        linkedList.insertFirst(98);
+        linkedList.insertFirst(700);
+        linkedList.insertFirst(78);
+        linkedList.insertFirst(955);
+        linkedList.insertLast(100);
+        //System.out.println(linkedList.isEmpty());
+        //linkedList.deleteFirst();
+        linkedList.displayList();
+    }
+}
