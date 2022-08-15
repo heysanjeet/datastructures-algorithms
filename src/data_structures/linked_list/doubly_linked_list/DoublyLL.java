91b86e1583b11b79868cbbe6c7d94bdc76e4467f
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2022-08-15 20:52:18 +0530

    implementation of DoublyLinkedList.

--- src/data_structures/linked_list/doubly_linked_list/DoublyLL.java
+++ src/data_structures/linked_list/doubly_linked_list/DoublyLL.java
@@ -1,4 +1,39 @@
 package data_structures.linked_list.doubly_linked_list;
 
 public class DoublyLL {
+    private Node first;
+    private Node last;
+
+
+    public boolean isEmpty() {
+        return (first == null);
+    }
+
+    public void insertFirst(int data) {
+        Node newNode = new Node();
+        newNode.data = data;
+
+        if (isEmpty()) {
+            last = newNode;//if empty, last node will refer to the new node being created.
+        } else {
+            first.previous = newNode;
+        }
+        newNode.next = first;//the new node next field will point to the old first
+        this.first = newNode;
+    }
+
+    public void insertLast(int data) {
+        Node newNode = new Node();
+        newNode.data = data;
+
+        if (isEmpty()) {
+            first = newNode;
+        } else {
+            last.next = newNode;//assigning old last to newNode
+            newNode.previous = last;//the old last node will be newNode previous
+        }
+        last = newNode;//the linked list last field should point to the new node
+    }
+
+
 }
