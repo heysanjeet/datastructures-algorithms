639a9d66e45fc6ce105d586cdb4d90548e9e6773
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-02-09 00:14:53 +0530

    implementation to find GCD.

--- src/sample_problems/FindFactorial.java
+++ src/sample_problems/FindFactorial.java
@@ -16,7 +16,7 @@ public class FindFactorial {
         }
         return (num * recursiveApproachForFactorial(num - 1));
     }
-
+    //it is not efficient solution, there stackoverflow issue
     public static int countZeroFromFactorial(int num) {
         int fact = 1;
         for (int i = 2; i <= num; i++) {
@@ -30,7 +30,15 @@ public class FindFactorial {
         return countZero;
     }
 
+    public static int countZeroFromFactorialEfficient(int num) {
+        int result = 0;
+        for (int i = 5; i <= num; i = i * 5) {
+            result = result + num / i;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println(countZeroFromFactorial(10));
+        System.out.println(countZeroFromFactorialEfficient(251));
     }
 }
