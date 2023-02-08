4fe78db67c2ce0365212a780483c0b00a14e51dc
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-02-08 22:41:10 +0530

    implementation to check if it Palindrome Number.

---
+++ src/sample_problems/PalindromeNumber.java
@@ -0,0 +1,18 @@
+package sample_problems;
+
+public class PalindromeNumber {
+    public static boolean isPalindrome(int number) {
+        int rev = 0;
+        int temp=number;
+        while (temp != 0) {
+            int reminder = temp % 10;
+            rev = rev * 10 + reminder;
+            temp = temp / 10;
+        }
+        return (rev==number);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+    }
+}
