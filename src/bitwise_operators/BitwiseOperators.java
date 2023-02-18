62257239bf96088ddbc554ea9970ab2c64cf1bba
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-02-19 00:01:32 +0530

    implementation for bitwise operators.

---
+++ src/bitwise_operators/BitwiseOperators.java
@@ -0,0 +1,9 @@
+package bitwise_operators;
+
+public class BitwiseOperators {
+    public static void main(String[] args) {
+        int x=3;
+        int y=6;
+        System.out.println(x&y);
+    }
+}
