5cf229bacdeecc40190df32d03dfa22a7e162d7a
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-02-23 13:00:42 +0530

    Stack implementation using LinkedList.

---
+++ src/data_structures/stacks/implementation_using_LL/Node.java
@@ -0,0 +1,11 @@
+package data_structures.stacks.implementation_using_LL;
+
+public class Node {
+    public int data;
+    public Node next;
+
+    public Node(int data) {
+        this.data = data;
+        next = null;
+    }
+}
---
+++ src/data_structures/stacks/implementation_using_LL/StackLLClient.java
@@ -0,0 +1,20 @@
+package data_structures.stacks.implementation_using_LL;
+
+public class StackLLClient {
+    public static void main(String[] args) {
+        StackLL stackLL=new StackLL();
+        stackLL.push(24);
+        stackLL.push(56);
+        stackLL.push(76);
+
+        System.out.println(stackLL.size);
+        System.out.println(stackLL.isEmpty());
+        System.out.println(stackLL.peek());
+
+        stackLL.pop();
+
+        System.out.println(stackLL.size);
+        System.out.println(stackLL.isEmpty());
+        System.out.println(stackLL.peek());
+    }
+}
