a5afb102c8c06750227817fac753352bf9d77c42
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-02-26 23:23:24 +0530

    Stack in collection.

--- src/data_structures/java_collections/Stack.java
+++ src/data_structures/java_collections/Stack.java
@@ -4,6 +4,7 @@ import java.util.ArrayDeque;
 
 public class Stack {
     public static void main(String[] args) {
+        //java.util.Stack stack=new java.util.Stack();//for multithreaded env.
         ArrayDeque<Integer> arrayDeque=new ArrayDeque<>();//it not thread safe better to use in single threaded env.
         arrayDeque.push(34);
         arrayDeque.push(54);
