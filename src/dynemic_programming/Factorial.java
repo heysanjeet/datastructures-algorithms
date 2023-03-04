c3bbfe874e9c1922b3fac2f16ad774a12bfdb923
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-03-05 00:05:37 +0530

    Find factorial using tail recursion.

--- src/dynemic_programming/Factorial.java
+++ src/dynemic_programming/Factorial.java
@@ -10,7 +10,15 @@ public class Factorial {
         }
     }
 
+    public static int findFacotrialTailRecurseion(int n,int k) {
+        if (n == 0) {
+            return k;
+        } else {
+            return findFacotrialTailRecurseion(n - 1, k*n);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(findFacotrial(5));
+        System.out.println(findFacotrialTailRecurseion(5,1));
     }
 }
