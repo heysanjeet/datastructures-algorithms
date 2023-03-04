508d9ac178bb49cec1ca578366d18158953f2a2a
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-03-05 00:28:20 +0530

    Fibonacci recursion.

---
+++ src/dynemic_programming/Fibonacci.java
@@ -0,0 +1,16 @@
+package dynemic_programming;
+
+public class Fibonacci {
+
+    public static int findFabonaccu(int n){
+        if(n<=1){
+            return n;
+        }
+
+        return findFabonaccu(n-1)+findFabonaccu(n-2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findFabonaccu(10));
+    }
+}
