53e42cc43d9e919aaf6cde4dd8d04269e1e4bd52
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-03-05 12:30:34 +0530

    Check if the String is Palindrome using recursion.

---
+++ src/dynemic_programming/Palindrome.java
@@ -0,0 +1,17 @@
+package dynemic_programming;
+
+import java.util.Stack;
+
+public class Palindrome {
+    public static boolean isPlalindrome(String str, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+        return (str.charAt(start) == str.charAt(end) && isPlalindrome(str, start + 1, end - 1));
+    }
+
+    public static void main(String[] args) {
+        String str="aacaa";
+        System.out.println(isPlalindrome(str,0,str.length()-1));//true
+    }
+}
