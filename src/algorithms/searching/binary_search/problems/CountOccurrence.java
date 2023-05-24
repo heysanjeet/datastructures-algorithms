a38446ece7e3d2c29a170ea35a1aa77f08ab01e4
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-05-24 10:33:10 +0530

    implementation of binary search[Find Index of count Occurrence].

---
+++ src/algorithms/searching/binary_search/problems/CountOccurrence.java
@@ -0,0 +1,7 @@
+package algorithms.searching.binary_search.problems;
+
+public class CountOccurrence {
+    public static int countOccurrence(int[] array, int target) {
+        return -1;
+    }
+}
