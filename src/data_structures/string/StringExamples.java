5f070a09e04c1f8654762b9b74c7664e4a7dbea4
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-05-31 18:42:03 +0530

    implementation of String[].

---
+++ src/data_structures/string/StringExamples.java
@@ -0,0 +1,18 @@
+package data_structures.string;
+
+public class StringExamples {
+    public static void main(String[] args) {
+
+        String s1="Sanjeet";
+        String s2=s1;
+        s1="Sanjeet Kumar";
+        s2=s1;
+        System.out.println(s1);
+        System.out.println(s1==s2);
+
+        //to print ascci value
+        char a='A';
+        System.out.println((int)a);
+
+    }
+}
