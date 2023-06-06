ea81d3bd9d684fbaa8f13489d115d00d0cbeb7f5
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-06-06 22:40:56 +0530

    squire root using binary search.

--- src/algorithms/searching/binary_search/problems/SqareRoot.java
+++ src/algorithms/searching/binary_search/problems/SqareRoot.java
@@ -2,7 +2,7 @@ package algorithms.searching.binary_search.problems;
 
 public class SqareRoot {
     public static void main(String[] args) {
-        System.out.println(findSqareRootOfN(25));//5
+        System.out.println(findSqare(9));//3
     }
 
     //Naive sol
@@ -13,5 +13,25 @@ public class SqareRoot {
 
         }
         return i - 1;
+    }
+
+    public static int findSqare(int x) {
+        int low = 0;
+        int high = x;
+        int result = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int midSqrt = mid * mid;
+            if (midSqrt == x) {
+                return mid;
+            }
+            if (midSqrt < x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+                result = mid;
+            }
+        }
+        return result;
     }
 }
