08c765abc7d12ce636f8ad2adf16553cfba74989
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-07-02 00:21:57 +0530

    count value from the sorted array.

--- src/data_structures/array/Array.java
+++ src/data_structures/array/Array.java
@@ -4,8 +4,8 @@ public class Array {
     public static void main(String[] args) {
         char[] array = {'a', 'b', 'c', 'd'};
         //findValue(array);
-        int[] intArray={5,4,7,3,6,4,5,2,2,7,3,9,4,7,4};
-        System.out.println(searchValue(intArray, 5));//0
+        int[] intArray = {5, 4, 7, 3, 6, 4, 5, 2, 2, 7, 3, 9, 4, 7, 4};
+        System.out.println(countValue(intArray, 5));//2
 
     }
 
@@ -14,12 +14,22 @@ public class Array {
         System.out.println(array[3]);
     }
 
-    public static int searchValue(int[] array , int target){
-        for(int i=0;i<array.length;i++){
-            if(array[i]==target){
+    public static int searchValue(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
                 return i;
             }
         }
         return -1;
+    }
+
+    public static int countValue(int[] array, int target) {
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                count++;
+            }
+        }
+        return count;
     }
 }
