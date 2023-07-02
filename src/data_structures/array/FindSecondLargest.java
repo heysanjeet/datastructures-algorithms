be0075616223ed7ffeef051168eb656ac9104bc0
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-07-03 01:53:44 +0530

    find second Largest value from array

---
+++ src/data_structures/array/FindSecondLargest.java
@@ -0,0 +1,33 @@
+package data_structures.array;
+
+public class FindSecondLargest {
+    public static void main(String[] args) {
+        int[] array = {25, 64, 9, 9, 8, 53, 4, 231};
+        System.out.println(findSecondLargest(array));//1
+    }
+
+    public static int getLargest(int[] array) {
+        int res = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[res] < array[i]) {
+                res = i;
+            }
+        }
+        return res;
+    }
+
+    public static int findSecondLargest(int[] array) {
+        int largest = getLargest(array);
+        int res = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] != array[largest]) {
+                if (res == -1) {
+                    res = i;
+                } else if (array[res] < array[i]) {
+                    res = i;
+                }
+            }
+        }
+        return res;
+    }
+}
