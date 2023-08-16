021025bd17cae345936eb05e79e054af86c9e8fe
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-08-16 13:31:33 +0530

    remove duplicate from an array

---
+++ src/data_structures/array/RemoveDuplicateFromSortedArray.java
@@ -0,0 +1,27 @@
+package data_structures.array;
+
+public class RemoveDuplicateFromSortedArray {
+    public static void main(String[] args) {
+        int[] sortedArray = {10, 20, 20, 30, 30, 30, 40, 40, 40};
+        int result = removeDuplicateValues(sortedArray);
+        for (int i = 0; i < result; i++) {
+            System.out.print(sortedArray[i] + " ");
+        }
+    }
+
+    private static int removeDuplicateValues(int[] array) {
+        int[] temp = new int[array.length];
+        temp[0] = array[0];
+        int resSize = 1;
+        for (int i = 1; i < array.length; i++) {
+            if (temp[resSize - 1] != array[i]) {
+                temp[resSize] = array[i];
+                resSize++;
+            }
+        }
+        for (int i = 0; i < resSize; i++) {
+            array[i] = temp[i];
+        }
+        return resSize;
+    }
+}
