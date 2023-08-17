9686223bf280a444f23ae54b60b28c513090fb64
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-08-17 22:48:43 +0530

    shift all zero at the end

---
+++ src/data_structures/array/MoveZeroToEnd.java
@@ -0,0 +1,27 @@
+package data_structures.array;
+
+public class MoveZeroToEnd {
+    public static void main(String[] args) {
+        int[] inputArray = {1, 0, 3, 0, 4, 0, 5, 0, 3, 0, 0, 2, 0, 2};
+        int[] result = moveToEnd(inputArray);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+
+    //naive sol
+    private static int[] moveToEnd(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {//search for zero
+                for (int j = i + 1; j < array.length; j++) {
+                    if (array[j] != 0) {//search for non-zero
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+        return array;
+    }
+}
