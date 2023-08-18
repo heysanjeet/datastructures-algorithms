6cb447abcb2e7d6f035869ed5b4aa726dbf447a0
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-08-18 09:32:03 +0530

    Arrays[Find largest element]

--- src/data_structures/array/FindMaxValue.java
+++ src/data_structures/array/FindMaxValue.java
@@ -3,7 +3,23 @@ package data_structures.array;
 public class FindMaxValue {
     public static void main(String[] args) {
         int[] array = {56, 6, 43, 98, 57, 52, 3, 10};
-        System.out.println(findMaxValue(array));//98
+        System.out.println(getLargestNaive(array));//98-3
+    }
+
+    public static int getLargestNaive(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            boolean flag = true;
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] > array[i]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag == true) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public static int findMaxValue(int[] array) {
