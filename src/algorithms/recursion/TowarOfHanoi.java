d8f174cd68b45ea445146df3e1f7930636bcba47
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-10-01 12:09:52 +0530

    recursion[TOH]

---
+++ src/algorithms/recursion/TowarOfHanoi.java
@@ -0,0 +1,17 @@
+package algorithms.recursion;
+
+public class TowarOfHanoi {
+    public static void main(String[] args) {
+        toh(3, 'A', 'B', 'C');
+    }
+
+    public static void toh(int n, char A, char B, char C) {
+        if (n == 1) {
+            System.out.println("Move 1 from " + A + " to " + C);
+            return;
+        }
+        toh(n - 1, A, C, B);
+        System.out.println("Move " + n + " From " + A + " to " + C);
+        toh(n - 1, B, A, C);
+    }
+}
