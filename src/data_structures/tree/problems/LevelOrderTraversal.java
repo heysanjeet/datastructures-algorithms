defa1f3dd2be0a988aec9361b04a2b0cb605d150
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-10-03 22:42:50 +0530

    tree[Tree traversal]

---
+++ src/data_structures/tree/problems/LevelOrderTraversal.java
@@ -0,0 +1,33 @@
+package data_structures.tree.problems;
+
+import data_structures.tree.Node;
+
+public class LevelOrderTraversal {
+
+    //approach first find the height of the tree.
+    //then print node at distance k from the root.
+    //Naive solution
+    public void levelOrderTraversal(Node root, int k) {
+        k = getHightOfTree(root);
+        if (k == -1) {
+            return;
+        }
+        if (k == 0)
+            System.out.println(root.key + " ");
+        else
+            levelOrderTraversal(root.left, k - 1);
+        levelOrderTraversal(root.right, k - 1);
+    }
+
+    private int getHightOfTree(Node root) {
+        if (root == null) {
+            return -1;
+        }
+        return Math.max(getHightOfTree(root.left), getHightOfTree(root.right)) + 1;
+    }
+
+    //Optimized solution
+    public void levelOrderTraversalEfficient() {
+
+    }
+}
