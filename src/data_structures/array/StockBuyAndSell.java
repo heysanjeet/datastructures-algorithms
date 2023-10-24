cbbe81e5c1f7d80e524ce2ef3c710f9eb6ad24eb
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-10-24 22:23:30 +0530

    Arrays[Buy and Sell stock]

---
+++ src/data_structures/array/StockBuyAndSell.java
@@ -0,0 +1,30 @@
+package data_structures.array;
+
+public class StockBuyAndSell {
+    /* Input: arr[] = {100, 180, 260, 310, 40, 535, 695}
+     Output: 865
+     Explanation: Buy the stock on day 0 and sell it on day 3 => 310 – 100 = 210
+     Buy the stock on day 4 and sell it on day 6 => 695 – 40 = 655
+     Maximum Profit  = 210 + 655 = 865*/
+    public static void main(String[] args) {
+        int arr[] = {100, 180, 260, 310, 40, 535, 695};
+        System.out.println(maxProfit(arr, 0, arr.length - 1));
+    }
+
+    public static int maxProfit(int[] price, int start, int end) {
+        if (end <= start) {
+            return 0;
+        }
+        int profit = 0;
+        for (int i = start; i < end; i++) {
+            for (int j = i + 1; j <= end; j++) {
+                if (price[j] > price[i]) {
+                    int currentProfilt = price[j] - price[i] + maxProfit(price, start, i - 1) +
+                            maxProfit(price, j + 1, end);
+                    profit = Math.max(profit, currentProfilt);
+                }
+            }
+        }
+        return profit;
+    }
+}
