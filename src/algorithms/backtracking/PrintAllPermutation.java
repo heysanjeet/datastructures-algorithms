055e5e7685ad86aff9dd5ecb002c518845cd5fcd
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-11-05 23:56:01 +0530

    Backtracking

---
+++ src/algorithms/backtracking/PrintAllPermutation.java
@@ -0,0 +1,34 @@
+package algorithms.backtracking;
+
+public class PrintAllPermutation {
+    public static void main(String[] args) {
+        String str = "ABC";
+        permute(str, 0, str.length() - 1);
+    }
+
+    public static void permute(String str, int l, int r) {
+
+        if (l == r) {
+            if (str.contains("AB") == false) {
+                System.out.print(str + " ");
+            }
+            return;
+        } else {
+            for (int i = l; i <= r; i++) {
+                str = new String(swap(str, i, l));
+
+                permute(str, l + 1, r);
+
+                str = new String(swap(str, i, l));
+            }
+        }
+    }
+
+    public static char[] swap(String str, int i, int j) {
+        char ch[] = str.toCharArray();
+        char temp = ch[i];
+        ch[i] = ch[j];
+        ch[j] = temp;
+        return ch;
+    }
+}
