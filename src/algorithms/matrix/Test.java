b09e7409ab4c559127d1a7601b4f5487019bb32b
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-11-06 21:53:40 +0530

    Matrix

---
+++ src/algorithms/matrix/Test.java
@@ -0,0 +1,12 @@
+package algorithms.matrix;
+
+public class Test {
+    public static void main(String[] args) {
+        int[][] array = {{1, 2, 3,8,9}, {4, 5, 6}};
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+        }
+    }
+}
