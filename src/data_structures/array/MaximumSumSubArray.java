73e4c84890eef127fc23900055749a038ff0f668
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-11-08 23:48:43 +0530

    Arrays

--- src/data_structures/array/MaximumSumSubArray.java
+++ src/data_structures/array/MaximumSumSubArray.java
@@ -2,6 +2,20 @@ package data_structures.array;
 
 public class MaximumSumSubArray {
     public static void main(String[] args) {
+        int[] array = {1, 2, 3};
+        System.out.println(maxSumsubarrayNaive(array));//6
+    }
 
+    //should be contiguous element
+    public static int maxSumsubarrayNaive(int[] array) {
+        int result = array[0];
+        for (int i = 0; i < array.length; i++) {
+            int curr = 0;
+            for (int j = i; j < array.length; j++) {
+                curr = curr + array[j];
+                result = Math.max(result, curr);
+            }
+        }
+        return result;
     }
 }
