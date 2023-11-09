6fa5fa5aa7213009011234e2c54604c0fd3f4c8d
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-11-09 19:47:46 +0530

    Arrays

---
+++ src/data_structures/array/MaximumLengthEnvenOddSubArray.java
@@ -0,0 +1,15 @@
+package data_structures.array;
+
+public class MaximumLengthEnvenOddSubArray {
+    public static void main(String[] args) {
+
+    }
+
+   /* Input: a[] = {1, 2, 3, 4, 5, 7, 9}
+    Output: 5
+    Explanation:
+    The subarray {1, 2, 3, 4, 5} has alternating even and odd elements.*/
+    public static int maximumLengthEnvenOddSubArray(int array[]){
+
+    }
+}
