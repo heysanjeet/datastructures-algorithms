2f753c1793ae32168d61b30652c72f4d19aff40e
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-12-24 20:24:50 +0530

    Graph

--- src/data_structures/graph/BFSExample2.java
+++ src/data_structures/graph/BFSExample2.java
@@ -1,4 +1,62 @@
 package data_structures.graph;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BFSExample2 {
+    public static void main(String[] args) {
+        int V = 7;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>(V);
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<Integer>());
+
+        }
+        addEdge(adj, 0, 1);
+        addEdge(adj, 0, 2);
+        addEdge(adj, 2, 3);
+        addEdge(adj, 1, 3);
+        addEdge(adj, 1, 3);
+        addEdge(adj, 4, 5);
+        addEdge(adj, 5, 6);
+        addEdge(adj, 4, 6);
+        System.out.println("Breath First Traversal for disconnect graph");
+        dfsDisconnected(adj, V);
+    }
+
+    //Bfs for disconnected graph
+    public static void dfsDisconnected(ArrayList<ArrayList<Integer>> adj, int V) {
+        boolean[] visited = new boolean[V];
+        for (int i = 0; i < V; i++) {
+            visited[i] = false;
+        }
+        for (int i = 0; i < V; i++) {
+            if (visited[i] == false) {
+                bfs(adj, i, visited);
+            }
+        }
+    }
+
+    public static void bfs(ArrayList<ArrayList<Integer>> adj, int s, boolean[] visited) {
+        Queue<Integer> queue = new LinkedList<>();
+        visited[s] = true;
+        queue.add(s);
+
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+            System.out.print(u + " ");
+            for (int v : adj.get(u)) {
+                if (visited[v] == false) {
+                    visited[v] = true;
+                    queue.add(v);
+                }
+            }
+        }
+    }
+
+
+    private static void addEdge(ArrayList<ArrayList<Integer>> adj, int v, int u) {
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
 }
