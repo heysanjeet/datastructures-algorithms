2f8364f171a19080dd919caf028bf3276bef43e0
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2023-12-25 11:50:21 +0530

    Graph

--- src/algorithms/matrix/MatrixExample.java
+++ src/algorithms/matrix/MatrixExample.java
@@ -2,10 +2,26 @@ package algorithms.matrix;
 
 public class MatrixExample {
     public static void main(String[] args) {
-        int[][] array = {{1, 2, 3,8,9}, {4, 5, 6}};
+        array2D();
+
+    }
+
+    public static void print2DArray() {
+        int[][] array = {{1, 2, 3, 8, 9}, {4, 5, 6}};
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void array2D(){
+        int[][] array=new int[2][5];
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array[i].length;j++){
+                array[i][j]=10;
+                System.out.print(array[i][j]+" ");
             }
             System.out.println();
         }
