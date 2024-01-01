c13b2654a7901377178220584a6148892c6f0b34
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2024-01-02 01:48:12 +0530

    AlgoExpert[Array]

--- src/algoexpert/arrays/easy/NonConstructibleChange.java
+++ src/algoexpert/arrays/easy/NonConstructibleChange.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 
 public class NonConstructibleChange {
     public static void main(String[] args) {
-
+         //coins=[5,7,1,1,2,3,22]
+        //output=20
     }
 
+    //time O(nlogn) | space O(1)
     public int nonConstructibleChange(int[] coins) {
         Arrays.sort(coins);
         int currentChnageCreated = 0;
