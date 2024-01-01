e21f6a31bd2cf86787623bc1ba0e3f862c3c33c8
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2024-01-01 23:00:28 +0530

    Leetcode[Array]

--- src/leetcode/top150/array/easy/MergeSortedArray.java
+++ src/leetcode/top150/array/easy/MergeSortedArray.java
@@ -1,4 +1,5 @@
 package leetcode.top150.array.easy;
+
 import java.util.Arrays;
 
 public class MergeSortedArray {
@@ -6,16 +7,41 @@ public class MergeSortedArray {
     /*Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
     Output: [1,2,2,3,5,6]*/
     public static void main(String[] args) {
-
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        merge3(nums1, 3, nums2, 3);
+        for (int i = 0; i < nums1.length; i++) {
+            System.out.print(nums1[i] + " ");
+        }
     }
-    public void merge2(int[] nums1, int m, int[] nums2, int n){
 
+    public static void merge3(int[] nums1, int m, int[] nums2, int n) {
+        int r1 = m - 1;
+        int r2 = n - 1;
+        for (int w = m + n - 1; w >= 0; w--) {
+            if (r1 >= 0 && r2 >= 0) {
+                nums1[w] = nums1[r1] > nums2[r2] ? nums1[r1--] : nums2[r2--];
+            } else if (r1 >= 0) {
+                nums1[w] = nums1[r1--];
+            } else {
+                nums1[w] = nums2[r2--];
+            }
+        }
     }
+
     //time O(m+n)log(m+n) | space O(1)
-    public void mergeNaive(int[] nums1, int m, int[] nums2, int n){
-        for(int i=m, j=0;j<n;j++){
-            nums1[i]=nums2[j];
-            j++;
+    public static void merge2(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            nums1[i + m] = nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+
+    //time O(m+n)log(m+n) | space O(1)
+    public static void merge1(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = m, j = 0; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
         }
         Arrays.sort(nums1);
     }
