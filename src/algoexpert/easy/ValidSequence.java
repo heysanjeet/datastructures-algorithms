1f2f68c2f50174a3fc8ac6136791efcc55c9af44
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2024-01-01 21:45:53 +0530

    AlgoExpert[Array]

--- src/algoexpert/easy/ValidSequence.java
+++ src/algoexpert/easy/ValidSequence.java
@@ -8,9 +8,11 @@ public class ValidSequence {
         /*int[] array={1,2,3,4,5,6};
         int[] sequence={4,5};
         output= true*/
-        System.out.println(isValidSubsequence(Arrays.asList(1,2,3,4,5,6),Arrays.asList(4,5)));//true
+        System.out.println(isValidSubSequence2(Arrays.asList(1,2,3,4,5,6),Arrays.asList(4,5)));//true
 
     }
+
+    //time O(n) | space O(1), n is length of the array
     public static boolean isValidSubsequence(
             List<Integer> array, List<Integer> sequence) {
 
@@ -24,5 +26,18 @@ public class ValidSequence {
             arrIndex++;
         }
         return seqIndex == sequence.size();
+    }
+
+    //time O(n) | space O(1) - where n is the length of the array
+    public static boolean isValidSubSequence2(List<Integer> array, List<Integer> sequence){
+        int seqIndex=0;
+        for(int value: array){
+            if(seqIndex==sequence.size()){
+                break;
+            }if(sequence.get(seqIndex).equals(value)){
+                seqIndex++;
+            }
+        }
+        return seqIndex==sequence.size();
     }
 }
