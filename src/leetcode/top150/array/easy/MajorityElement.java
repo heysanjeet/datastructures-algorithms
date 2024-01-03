8ec7433c1ebaf515648c56c5ae3ee0e5e07ed31d
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2024-01-03 22:44:36 +0530

    Leetcode[Array]

--- src/leetcode/top150/array/easy/MajorityElement.java
+++ src/leetcode/top150/array/easy/MajorityElement.java
@@ -1,4 +1,33 @@
 package leetcode.top150.array.easy;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MajorityElement {
+
+    //time O(n) |space O(1)
+    public int majorityElementNaive(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+
+    //time O(n) | O(n)
+    public int majorityElement(int[] nums) {
+        int n=nums.length;
+        Map<Integer, Integer> map=new HashMap<Integer,Integer>();
+        for(int i=0;i<n;i++){
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+
+        }
+        n=n/2;
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            if(entry.getValue()>n){
+                return entry.getKey();
+            }
+        }
+        return 0;
+
+    }
+
 }
