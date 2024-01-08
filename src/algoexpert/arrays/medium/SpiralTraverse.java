7fcdbedbcc4b75e00ace3ce42407f787f48e5db2
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2024-01-09 00:42:55 +0530

    Array

---
+++ src/algoexpert/arrays/medium/SpiralTraverse.java
@@ -0,0 +1,48 @@
+package algoexpert.arrays.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralTraverse {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    //Time O(n) | space O(n) n is the total no of elements in the array
+    public static List<Integer> spiralTraverse(int[][] array) {
+        if (array.length == 0) {
+            return new ArrayList<>();
+        }
+        List<Integer> result = new ArrayList<>();
+        int startRow = 0;
+        int endRow = array.length - 1;
+        int startCol = 0;
+        int endCol = array[0].length - 1;
+
+        while (startRow <= endRow && startCol <= endCol) {
+            for (int col = startCol; col <= endCol; col++) {
+                result.add(array[startRow][col]);
+            }
+            for (int row = startRow + 1; row <= endRow; row++) {
+                result.add(array[row][endCol]);
+            }
+            for (int col = endCol - 1; col >= startCol; col--) {
+                if (startRow == endRow) break;
+                result.add(array[endRow][col]);
+            }
+            for (int row = endRow - 1; row > startRow; row--) {
+                if (startCol == endCol) break;
+                result.add(array[row][startCol]);
+            }
+            startRow++;
+            endRow--;
+            startCol++;
+            endCol--;
+
+        }
+        return result;
+    }
+}
+
