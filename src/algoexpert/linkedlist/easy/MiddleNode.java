95ade4608dde8e773220a3ca988232b8c624cfec
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2024-01-22 01:52:10 +0530

    LinkedList

--- src/algoexpert/linkedlist/easy/MiddleNode.java
+++ src/algoexpert/linkedlist/easy/MiddleNode.java
@@ -9,16 +9,28 @@ public class MiddleNode {
     }
 
     //Time O(n) | space O(1)
-    public LinkedList middleNode(LinkedList linkedList) {
-        int count=0;
-        LinkedList currentNode=linkedList;
-        while(currentNode !=null){
-            count++;
-            currentNode=currentNode.next;
+    public LinkedList middleNode2(LinkedList linkedList) {
+        LinkedList slowNode = linkedList;
+        LinkedList fastNode = linkedList;
+        while (fastNode != null && fastNode.next != null) {
+            slowNode = slowNode.next;
+            fastNode = fastNode.next.next;// moving double than slow node
         }
-        LinkedList middleNode=linkedList;
-        for(int i=0;i<count/2;i++){
-            middleNode=middleNode.next;
+        return slowNode;
+    }
+
+
+    //Time O(n) | space O(1)
+    public LinkedList middleNode(LinkedList linkedList) {
+        int count = 0;
+        LinkedList currentNode = linkedList;
+        while (currentNode != null) {
+            count++;
+            currentNode = currentNode.next;
+        }
+        LinkedList middleNode = linkedList;
+        for (int i = 0; i < count / 2; i++) {
+            middleNode = middleNode.next;
         }
         return middleNode;
     }
