99577a9d076774779e364747e0f9b14fdeb4c043
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2024-01-28 00:55:57 +0530

    Array

---
+++ src/algoexpert/arrays/medium/MissingNumbers.java
@@ -0,0 +1,30 @@
+package algoexpert.arrays.medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MissingNumbers {
+    public static void main(String[] args) {
+
+    }
+
+
+    //Time O(n) | space O(1)
+    public int[] missingNumbers(int[] nums) {
+        Set<Integer> includedNums = new HashSet<Integer>();
+        for (int num : nums) {
+            includedNums.add(num);
+        }
+        int[] solution = new int[]{-1, -1};
+        for (int num = 1; num < nums.length + 3; num++) {
+            if (!includedNums.contains(num)) {
+                if (solution[0] == -1) {
+                    solution[0] = num;
+                } else {
+                    solution[1] = num;
+                }
+            }
+        }
+        return solution;
+    }
+}
