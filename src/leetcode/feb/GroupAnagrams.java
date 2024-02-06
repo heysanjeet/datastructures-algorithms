754101e512ed7ecceb72c177abf4916b554403aa
Sanjeet Kumar <imsanjeetkr7@gmail.com>
2024-02-06 20:33:50 +0530

    Feb problems

--- src/leetcode/feb/GroupAnagrams.java
+++ src/leetcode/feb/GroupAnagrams.java
@@ -26,4 +26,17 @@ public class GroupAnagrams {
 
         return new ArrayList<>(map.values());
     }
+
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = new char[26];
+            for (char c : s.toCharArray()) ca[c - 'a']++;
+            String keyStr = String.valueOf(ca);
+            if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<>());
+            map.get(keyStr).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
 }
